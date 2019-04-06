eec3da4e2077c0f8049fb88f358d3bee2d28d926
Zawediz <48649392+Zawediz@users.noreply.github.com>
2019-04-06 22:20:03 +0300

    Исправление ошибок

--- CodeReview.py
+++ CodeReview.py
@@ -1,93 +1,98 @@
-from tkinter import *
+import tkinter
 
-n = 0
-auto = 0
-hands = 1
+
+class GameItems:
+    n = 0
+    auto = 0
+    hands = 1
 
 
 def click(event):
-    global n
-    n += hands
-    count['text'] = 'ВАШ СЧЁТ : ' + str(n) + '\n' + 'ВАШИ АВТОНАЖАТИЯ : ' + str(auto) + '\n' + 'У ВАС РУК : ' + str(
-        hands)
+    main_button.unbind_all('<space>')
+    game.n += game.hands
+    count['text'] = 'ВАШ СЧЁТ : ' + str(game.n) + '\n' + 'ВАШИ АВТОНАЖАТИЯ : ' + str(
+        game.auto) + '\n' + 'У ВАС РУК : ' + str(game.hands)
+    root.after(100, set_clicked)
 
 
-def PurchaseAuto():
-    global n
-    global auto
-    if n < 100:
-        ErrorLabel = Label(root, text='НЕ ХВАТАЕТ СЧЕТА ДЛЯ ПОКУПКИ', fg='red')
-        ErrorLabel.place(relx=0.3, rely=0)
+def set_clicked():
+    main_button.bind_all('<space>', click)
+
+
+def purchase_auto():
+    if game.n < 100:
+        error_label = tkinter.Label(root, text='НЕ ХВАТАЕТ СЧЕТА ДЛЯ ПОКУПКИ', fg='red')
+        error_label.place(relx=0.3, rely=0)
     else:
-        ErrorLabel = Label(root,
-                           text='                                                                        ')
-        ErrorLabel.place(relx=0.3, rely=0)
-        n -= 100
-        auto += 1
+        error_label = tkinter.Label(root,
+                                    text='                                                                        ')
+        error_label.place(relx=0.3, rely=0)
+        game.n -= 100
+        game.auto += 1
 
 
-def Auto():
-    global n
-    global auto
-    n += auto
-    count['text'] = 'ВАШ СЧЁТ : ' + str(n) + '\n' + 'ВАШИ АВТОНАЖАТИЯ : ' + str(auto) + '\n' + 'У ВАС РУК : ' + str(
-        hands)
-    root.after(1000, Auto)
+def auto_function():
+    game.n += game.auto
+    count['text'] = 'ВАШ СЧЁТ : ' + str(game.n) + '\n' + 'ВАШИ АВТОНАЖАТИЯ : ' + str(
+        game.auto) + '\n' + 'У ВАС РУК : ' + str(game.hands)
+    root.after(1000, auto_function)
 
 
-def PurchaseHand():
-    global n
-    global hands
-    if n < 50:
-        ErrorLabel = Label(root, text='НЕ ХВАТАЕТ СЧЕТА ДЛЯ ПОКУПКИ', fg='red')
-        ErrorLabel.place(relx=0.3, rely=0)
+def purchase_hand():
+    if game.n < 50:
+        error_label = tkinter.Label(root, text='НЕ ХВАТАЕТ СЧЕТА ДЛЯ ПОКУПКИ', fg='red')
+        error_label.place(relx=0.3, rely=0)
     else:
-        ErrorLabel = Label(root,
-                           text='                                                                        ')
-        ErrorLabel.place(relx=0.3, rely=0)
-        n -= 50
-        hands += 1
+        error_label = tkinter.Label(root,
+                                    text='                                                                        ')
+        error_label.place(relx=0.3, rely=0)
+        game.n -= 50
+        game.hands += 1
 
 
-root = Tk()
+root = tkinter.Tk()
 root.title('КЛИКЕР')
+game = GameItems()
 w = 500
 h = 500
-XCoordinate = (root.winfo_screenwidth() - w) / 2
-YCoordinate = (root.winfo_screenheight() - h) / 2
-root.geometry('%dx%d+%d+%d' % (w, h, XCoordinate, YCoordinate))
+x_coordinate = (root.winfo_screenwidth() - w) / 2
+y_coordinate = (root.winfo_screenheight() - h) / 2
+root.geometry('%dx%d+%d+%d' % (w, h, x_coordinate, y_coordinate))
 
-MainButtonPhoto = PhotoImage(file='red_button.gif')
-MainButton = Button(root, width=100, height=100, image=MainButtonPhoto)
-MainButton.bind_all('<space>', click)
-MainButton.place(relx=.4, rely=.3)
+main_button_photo = tkinter.PhotoImage(file='red_button.gif')
+main_button = tkinter.Button(root, width=100, height=100, image=main_button_photo)
+main_button.bind_all('<space>', click)
+main_button.place(relx=.4, rely=.3)
 
-MainButtonLabel = Label(root, text='ЖМИ НА ПРОБЕЛ', fg='purple')
-MainButtonLabel.place(relx=0.4, rely=0.25)
+main_button_label = tkinter.Label(root, text='ЖМИ НА ПРОБЕЛ', fg='purple')
+main_button_label.place(relx=0.4, rely=0.25)
 
-QuitButtonPhoto = PhotoImage(file='exit_button.gif')
-QuitButton = Button(root, text='ВЫХОД', command=root.quit, width=50, height=25, image=QuitButtonPhoto)
-QuitButton.place(relx=0, rely=0)
+quit_button_photo = tkinter.PhotoImage(file='exit_button.gif')
+quit_button = tkinter.Button(root, text='ВЫХОД', command=root.quit, width=50, height=25, image=quit_button_photo)
+quit_button.place(relx=0, rely=0)
 
-count = Label(root,
-              text='ВАШ СЧЁТ : ' + str(n) + '\n' + 'ВАШИ АВТОНАЖАТИЯ : ' + str(auto) + '\n' + 'У ВАС РУК : ' + str(
-                  hands), fg='green')
+count = tkinter.Label(root,
+                      text='ВАШ СЧЁТ : ' + str(game.n) + '\n' + 'ВАШИ АВТОНАЖАТИЯ : ' + str(
+                          game.auto) + '\n' + 'У ВАС РУК : ' + str(
+                          game.hands), fg='green')
 count.place(relx=0.7, rely=0)
 
-AutoUpgradeButtonPhoto = PhotoImage(file='blue_button.gif')
-AutoUpgradeButton = Button(root, command=PurchaseAuto, width=100, height=100, image=AutoUpgradeButtonPhoto)
-AutoUpgradeButton.place(relx=0.05, rely=0.8)
+auto_upgrade_button_photo = tkinter.PhotoImage(file='blue_button.gif')
+auto_upgrade_button = tkinter.Button(root, command=purchase_auto, width=100, height=100,
+                                     image=auto_upgrade_button_photo)
+auto_upgrade_button.place(relx=0.05, rely=0.8)
 
-AutoUpgradeButtonLabel = Label(root, text='КУПИТЬ АВТОНАЖАТИЕ ЗА 100', fg='blue')
-AutoUpgradeButtonLabel.place(relx=0, rely=0.75)
+auto_upgrade_button_label = tkinter.Label(root, text='КУПИТЬ АВТОНАЖАТИЕ ЗА 100', fg='blue')
+auto_upgrade_button_label.place(relx=0, rely=0.75)
 
-Auto()
+auto_function()
 
-HandUpgradeButtonPhoto = PhotoImage(file='hand.gif')
-HandUpgradeButton = Button(root, command=PurchaseHand, width=100, height=100, image=HandUpgradeButtonPhoto)
-HandUpgradeButton.place(relx=0.75, rely=0.8)
+hand_upgrade_button_photo = tkinter.PhotoImage(file='hand.gif')
+hand_upgrade_button = tkinter.Button(root, command=purchase_hand, width=100, height=100,
+                                     image=hand_upgrade_button_photo)
+hand_upgrade_button.place(relx=0.75, rely=0.8)
 
-HandUpgradeButtonLabel = Label(root, text='КУПИТЬ РУКУ ЗА 50', fg='brown')
-HandUpgradeButtonLabel.place(relx=0.75, rely=0.75)
+hand_upgrade_button_label = tkinter.Label(root, text='КУПИТЬ РУКУ ЗА 50', fg='brown')
+hand_upgrade_button_label.place(relx=0.75, rely=0.75)
 
 root.mainloop()
